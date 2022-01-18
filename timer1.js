9ec762c47b18b6b9abdd7fe0d1872cf522c04d8d
knagayama76 <knagayama76>
2022-01-18 16:06:09 -0500

    timer created

---
+++ timer1.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const args = process.argv.slice(2);
+
+for (const time of args) {
+  if (args.length === 0) return;
+
+  if (time < 0) {
+    continue;
+  }
+
+  if (time.isNan) {
+    continue;
+  }
+
+  setTimeout(() => {
+    process.stdout.write(".");
+  }, time * 1000);
+}
